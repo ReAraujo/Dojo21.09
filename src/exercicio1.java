42b203c07224440a044618eb6d13b03808640743
Joanne PSilva <joannepassos@gmail.com>
2021-09-21 09:52:23 -0300

    Implementação do menu

--- src/exercicio1.java
+++ src/exercicio1.java
@@ -1,3 +1,32 @@
-public class exercicio1 {
+import java.util.Scanner;
 
+public class exercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+
+
+        do{
+            System.out.println("Escolha uma opção:\n 1 - Números pares \n 2 - Números ìmpares \n 3 - Números Primos \n 4 - Investimento \n 5 - Sair");
+            opcao = scanner.nextInt();
+            switch (opcao){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+
+                default:
+                    System.out.println("______Opção inválida______");
+                    System.out.println("Digite novamente");
+
+            }
+        }while (opcao <=0 || opcao > 5);
+    }
 }
